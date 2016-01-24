bf3455f7dd97fe16f0de23cce6af788fdd36d572
Oopsno <yufeiliu94@gmail.com>
2016-01-24 23:38:50 +0800

    added `init_{dictionary|configuration}`

--- src/fs.js
+++ src/fs.js
@@ -1,18 +1,38 @@
 var os = require('os');
 var fs = require('fs');
 var path = require('path');
+var log4js = require('log4js');
+var logger = log4js.getLogger('prprman.fs');
 const homedir = os.homedir();
 const prpr_root = path.join(homedir, '.prprman');
 const prpr_index = path.join(prpr_root, 'index');
+const prpr_config = path.join(prpr_root, 'prpr.json');
+const default_config = {
+  'source': []
+};
 
 const init_path = [prpr_root, prpr_index];
 
-function init() {
-  init_path.forEach(function (path) {
+function init_configuration() {
+  if (!fs.existsSync(prpr_config)) {
+    fs.writeFileSync(prpr_config, JSON.stringify(default_config));
+    logger.info('Written default configuration to:', prpr_config);
+  }
+}
+
+function init_dictionary() {
+  for (var i = 0; i < init_path.length; i++) {
+    var path = init_path[i];
     if (!fs.existsSync(path)) {
       fs.mkdirSync(path);
+      logger.info('Created local dictionary:', path);
     }
-  });
+  }
+}
+
+function init() {
+  init_dictionary();
+  init_configuration();
 }
 
 exports.init = init;
